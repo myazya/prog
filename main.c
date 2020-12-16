223c1cc32acb90c1ff4f6e6050f8c61911f74ad2
myazya <71670055+myazya@users.noreply.github.com>
2020-12-17 02:39:08 +0300

    lab4.2
    2. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
    значения π использовать 3.14.

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 //
 //  main.c
-//  201-722_Myaziana_4.1
+//  201-722_Myazina_4.2
 //
+//  Created by MacBook on 07.10.2020.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int a, b;
-    int s, p;
-    printf("Введите сторону а:\n");
-    scanf("%d/n", &a);
-    printf("Введите сторону b:\n");
-    scanf("%d/n", &b);
-    s = a * b;
-    p = 2 * (a + b);
-    printf("Площадь = %d\n", s);
-    printf("Периметр = %d\n", p);
+    int d;
+    float  l;
+    scanf ("%d", &d);
+    l = 3.14 * d;
+    printf("Длина окружности = %.2f\n", l);
     return 0;
 }
